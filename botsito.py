7dc6afc418a294f2e1283511cdbabe8ba5e61802
alone07 <80437440+alone07@users.noreply.github.com>
2021-04-26 19:46:42 -0500

    Add files via upload

---
+++ botsito.py
@@ -0,0 +1,14 @@
+import pyautogui as pg
+import time
+import webbrowser as web
+phone_no="+573006872350"
+phone_no="+573006872350"
+parsedMessage=""
+web.open('https://web.whatsapp.com/send?phone='+phone_no+'&text='+parsedMessage)
+time.sleep(8)
+for i in range(100):
+    pg.write("cuantos son") 
+    pg.press('enter')
+    pass
+  
+    
